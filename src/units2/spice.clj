2c2d82d1f5fe801854ff198c974052d7b7ea7a4f
mfey <mfeyer@wcw-self-145-109-125-237.wireless.uva.nl>
2016-04-07 10:59:11 +0200

    Fixed spice.clj
    Fixing typos and adjusting numerical quantities.

--- src/units2/spice.clj
+++ src/units2/spice.clj
@@ -5,19 +5,18 @@
   (:import (javax.measure.unit Unit SI))
 )
 
-(defunit-with-SI-prefixes g (->IfnUnit SI/GRAM))
+(defunit-with-SI-prefixes g (->IFnUnit SI/GRAM))
 
 ;; capsaisinoid concentrations can be determined to parts per million with liquid chromatography
-(defunit ppm (divide g Mg))
+(defunit ppm (divide mug g))
 
 ;; The Scoville is a historical unit for spice of roughly 15 or 16 ppm pure capsaisin
-(def history 15)
-(defunit-with-SI-prefixes scoville (recsale ppm history))
+(def history (/ 15))
+(defunit-with-SI-prefixes scoville (rescale ppm history))
 
 ;; consistency check: pure capsaicin is 1,000,000 ppm capsaicin
-(def pure-capsaicin (Mscoville history))
-(ppm pure-capsaicin)
-(Math/log10 (getValue pure-capsaicin ppm)) ;; protip: never count zeros manually
+(def pure-capsaicin (ppm 1e6))
+(Mscoville pure-capsaicin)
 
 ;; time to cook!
 
@@ -36,29 +35,33 @@
 (def rice (scoville 10)) ; very lightly spiced because the same spoon was used for the curry.
 
 ;; let's see how much spicier habaneros are than jalapenos
-(ops// (:habanero pepper) (:jalapeno pepper))
+(ops/divide-into-double (:habanero pepper) (:jalapeno pepper))
 
 ;; it's easy to check that the Old Spice Man's pungency is over 9000 (in the appropriate units):
 (ops/> (:oldspiceman meat) (kscoville 9000))
 
-;; now, let's *actually* cook!
+;; ;; now, let's *actually* cook!
 
 ;; concentrations (c) are intensive quantities... we need to say how much weight (w) of each ingredient we're adding...
 (defn add-ingredients [[c1 w1] [c2 w2]]
   (ops/with-unit-arithmetic
     (let [w (+ w1 w2)]
-      [(/ (+ (* c1 cw) (* c2 w2)) w) w])))
+      [(/ (+ (* c1 w1) (* c2 w2)) w) w])))
 
-;; which is spiciest? 
-(add-ingredients [(:jalapeno pepper) (g 5)] [(:chicken meat) (g 500)]])
-(add-ingredients [(:lamb meat) (g 50)] [rice (kg 1)])
+;; which is spiciest?
+(let [recipe1 (add-ingredients [(:jalapeno pepper) (g 5)] [(:chicken meat) (kg 0.5)])
+      recipe2 (add-ingredients [(:lamb meat) (g 50)] [rice (kg 1)])
+     ]
+  (map #(getValue % scoville) [(first recipe1) (first recipe2)])
+)
 
 ;; a bit of everything... imagine managing units by hand!!!
 (reduce add-ingredients [
-  [(:pepperoncino pepper) (g 50)]
+  [(:pepperoncino pepper) (g 150)]
   [(:habanero pepper) (g 10)]
-  [(:chicken meat) (g 200)]
-  [(:lamb meat) (g 300)]
+  [rice (g 600)]
+  [(:chicken meat) (g 240)]
+  [(:lamb meat) (kg 0.3)]
 ])
 
 ;; we need to counteract all the heat with dairy products. Let's model this with negative quantities in scoville units...
@@ -66,9 +69,9 @@
 (def milk (kscoville (- (rand)))) ;; some unknown value (not representative of reality)
 
 ;; there's no sensible interpretation as negative capsaisinoid concentration, so we'll have to do some work to
-;; determine these amounts empirically: keep mouthwashing milk until it's not spicy.
+;; determine these amounts empirically: keep mouthwashing with milk until it's not spicy.
 
-(def process (iterate (fn [x] (add-ingredients [milk (g 10)] x)) [(:jalapeno pepper) (g 1)]))
+(def process (iterate (fn [x] (add-ingredients [milk (g 100)] x)) [(:jalapeno pepper) (g 1.4)]))
 (def success (first (filter #(neg? (getValue (first %) scoville)) process)))
 
 ;; we can use this and the concentration-weight relation to estimate the soothingness of milk:
@@ -81,7 +84,6 @@
        ]
   (ops/with-unit-arithmetic
     (/ (- (* cmix wmix) (* cj wj)) (- wmix wj)))))
-))
 
-;; how far off was our empirical estimation?
-(ops// (ops/- estimated-milk milk) milk)
+;; how far from the true value is our empirical estimation?
+(Math/abs (ops/divide-into-double (ops/- estimated-milk milk) milk))
